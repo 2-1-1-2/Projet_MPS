6136a59f8b0b8c45d0a1ad3cab7dbd0c3fd65910
2-1-1-2 <tanyan.francois@gmail.com>
2024-02-28 14:12:29 +0100

    scene hpp

--- src/Scene.hpp
+++ src/Scene.hpp
@@ -1,12 +1,42 @@
+#include "Flock.hpp"
+#include "p6/p6.h"
 class Scene {
     /*
     - Taille de la scene
     - Position de la scene
 
     */
+
 private:
+    p6::Context ctx = p6::Context{{.title = "Projet_MPS"}};
+    Flock       flocks;
+
 public:
-    Scene()
+    explicit Scene(int nb)
+        : flocks(nb)
     {
+        ctx.maximize_window();
     }
+
+    void update()
+    {
+        ctx.update = [&]() {
+            ctx.background(p6::NamedColor::Blue);
+            renderFlock();
+            flocks.move();
+        };
+    }
+
+    void renderFlock()
+    {
+        for (auto& b : flocks.getBoids()) // access by reference to avoid copying
+        {
+            ctx.circle(
+                p6::Center{b.getPos()},
+                p6::Radius{0.025f}
+            );
+        }
+    }
+
+    void start() { ctx.start(); }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <cstdlib>
-#include "Flock.hpp"
 #include "glm/fwd.hpp"
 #define DOCTEST_CONFIG_IMPLEMENT
+#include "Scene.hpp"
 #include "doctest/doctest.h"
 #include "p6/p6.h"
 
@@ -12,26 +12,7 @@ int main()
     if (doctest::Context{}.run() != 0)
         return EXIT_FAILURE;
 
-    // Actual application code
-    auto ctx = p6::Context{{.title = "Projet_MPS"}};
-    ctx.maximize_window();
-
-    Flock f(30);
-
-    // Declare your infinite update loop.
-    ctx.update = [&]() {
-        ctx.background(p6::NamedColor::Blue);
-
-        for (auto& b : f.getBoids()) // access by reference to avoid copying
-        {
-            ctx.circle(
-                p6::Center{b.getPos()},
-                p6::Radius{0.025f}
-            );
-        }
-        f.move();
-    };
-
-    // Should be done last. It starts the infinite loop.
-    ctx.start();
+    Scene scene(30);
+    scene.update();
+    scene.start();
 }
