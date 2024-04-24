cb54fed7e792f9646ac280a911ac4ff6687ef038
2-1-1-2 <tanyan.francois@gmail.com>
2024-04-24 11:27:04 +0200

    Début fonction maths

---
+++ doc/Pre-rapport Maths.md
@@ -0,0 +1,41 @@
+# Contraintes théoriques :
+L’implémentation doit réaliser la simulation d’au mininum huitariables aléatoires.
+
+- [x]  Les lois des variables aléatoires doivent être toutes différentes.
+- [x] Toutes les variables aléatoires utilisées doivent être de loi connues.
+    - [ ] Loi Bêta
+    - [x] Loi de Bernouilli
+    - [x] Loi Binomiale
+    - [ ] Loi Exponentielle
+    - [ ] Loi Hypergéométrique
+    - [ ] Loi Normale
+    - [x] Loi Uniforme (discrète et continue)
+    - [ ] La Chaine de Markov
+
+- [x] Au moins quatre des variables aléatoires doivent être à densité par rapport à la mesure de Lebesgue.
+    - Loi Exponentielle
+    - Loi Normale
+    - Loi Uniforme continue
+    - Loi Bêta
+
+- [x] Au moins l’une des variables aléatoires ne doit pas prendre ses valeurs dans un ensemble numérique.
+Numérique
+    - Coordonnées emplacements tombe
+    - Nombre de tombes
+    - Nombre de mains à chaque vague
+    - La n-ième tombe qui sort
+    - Temps entre les vagues de mains
+    - Vitesse feu follet
+    - Météo - Luminosité
+
+Qualitative
+    - Modèle du personnagee
+    - Couleurs feu follet
+    
+
+
+- [ ] La connaissance de l’espérance et de la variance des variables aléatoires à valeurs numériques qui sont utilisées est requise.
+
+- [ ] Vous devez avoir la maîtrise de l’ensemble des paramètres régissant les lois des variables aléatoires utilisées.
+
+- [ ] Essayez de rendre possible la possibilité d e consulter quelques données statistiques : écart à la moyenne, histogramme, etc. Dans le cas où cela est possible et pertinent on pourra comparer ces indicateurs aux résultats théoriques attendus
--- src/Maths/Math.hpp
+++ src/Maths/Math.hpp
@@ -1,24 +1,115 @@
 #pragma once
+#include <math.h>
 #include <cmath>
 #include <cstdlib>
+#include <iostream>
+#include <random>
+
 class Math {
 private:
 public:
     // TODO(tanya): voir pour faire un intervalle
-    static float randomFloat()
-    {
-        constexpr int N   = 10000;
-        float         res = 0;
-        for (int i = 1; i < N; i++)
+    /*     static float randomFloat()
         {
-            res += static_cast<float>(rand() % 2) / static_cast<float>(pow(2, i));
-        }
+            constexpr int N   = 10000;
+            float         res = 0;
+            for (int i = 1; i < N; i++)
+            {
+                res += static_cast<float>(rand() % 2) / static_cast<float>(pow(2, i));
+            }
 
-        return res;
+            return res;
+        } */
+
+    static float rand()
+    {
+        static std::random_device                    rand;
+        static std::mt19937                          gen(rand());
+        static std::uniform_real_distribution<float> dis(0.0, 1.0);
+
+        return dis(gen);
+    }
+    static float randomFloat(float min = 0, float max = 1)
+    {
+        if (min > max)
+        {
+            std::swap(min, max);
+        }
+        return min + rand() * (max - min);
+    }
+
+    static int randomInt(int min = 0, int max = 1)
+    {
+        return round(randomFloat(min, max));
     }
 
     static int randomSign()
     {
-        return (rand() % 2) == 0 ? 1 : -1;
+        return (randomInt() % 2) == 0 ? 1 : -1;
+    }
+
+    static float randUniformD(int min, int max)
+    {
+        return randomInt(min, max);
+    }
+
+    static float randUniformC(int min, int max)
+    {
+        return randomFloat(min, max);
+    }
+
+    static bool randBernouilli(float proba)
+    {
+        static const float x = randomFloat();
+        toString("Binomial", proba, x);
+        return x <= proba;
+    }
+
+    static float randBinomial(float proba, float n)
+    {
+        static int success;
+        for (unsigned int i = 0; i < n; i++)
+        {
+            if (randBernouilli(proba))
+            {
+                success += 1;
+            }
+        }
+        return success;
+    }
+
+    static float randExponential(float lambda)
+    {
+        /* float proba = lambda * exp((-lambda) * t);  */ // proba de x > t
+        return 1;
+    }
+
+    static float randBeta()
+    {
+        return 0;
+    }
+
+    static float randHyperGeom()
+    {
+        return 0;
+    }
+
+    static float randNormale()
+    {
+        return 0;
+    }
+
+    static void toString(std::string name, float proba, float tirage)
+    {
+        std::cout << "Loi" << name << "\nProba : " << proba << "\nTirage : " << tirage << "\n\n\n";
+    }
+
+    static float esperance()
+    {
+        return 0;
+    }
+
+    static void allTest()
+    {
     }
 };
